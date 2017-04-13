e755af86f89db88bd5997694d0bd5ba55ff5c474
Pawel Rawicki <pawel.rawicki@gmail.com>
2017-04-13 11:56:22 +0200

    jeszcze test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<string>
 #include<iostream>
 #include <cstdlib>
+#include <vector>
 
 #include <tchar.h>
 #include "divpanc.h"
@@ -11,16 +12,13 @@ using namespace std;
 
 int main()
 {
-	cout << "aaaabbbbbbbbbbbbbbbbbbbbbbbbbbssssssssssss" << endl;
-	cout << "dddd" << endl;
-	cout << "ssss" < endl;
-	cout << "sssssssasa" << endl;
-	cout << "eeeeee" << endl;
+	
 //tescik();
 //static Armia a(1,3);
 //Armia d(4, 3);
-
+	cout << "aaaa" << endl<<endl;
 //a["aaaaaaaa"];
+	vector <Armia> armijka;
 	Armia a(3, 4);
 	Armia b(2, 2);
 	bool c;
